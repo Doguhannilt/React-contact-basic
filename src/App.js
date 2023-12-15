f35ab02deaf71ccd8122007a69c7b2c473d1655f
Doğuhan İlter <doguhannilt@gmail.com>
2023-12-15 06:19:13 +0300

    Basic React Contacts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const ContactApp = () => {
+  const [contacts, setContacts] = useState([
+    { id: 1, name: 'John Doe', email: 'john@example.com' },
+    { id: 2, name: 'Jane Doe', email: 'jane@example.com' },
+  ]);
+
+  const [newContact, setNewContact] = useState({ id: '', name: '', email: '' });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewContact({ ...newContact, [name]: value });
+  };
+
+  const handleAddContact = () => {
+    setContacts([...contacts, newContact]);
+    setNewContact({ id: '', name: '', email: '' });
+  };
+
+  const handleDeleteContact = (id) => {
+    const updatedContacts = contacts.filter(contact => contact.id !== id);
+    setContacts(updatedContacts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contact List</h1>
+      <ul>
+        {contacts.map(contact => (
+          <li key={contact.id}>
+            {contact.name} - {contact.email}
+            <button onClick={() => handleDeleteContact(contact.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Add New Contact</h2>
+      <label>ID:</label>
+      <input
+        type="text"
+        name="id"
+        value={newContact.id}
+        onChange={handleInputChange}
+      />
+      <label>Name:</label>
+      <input
+        type="text"
+        name="name"
+        value={newContact.name}
+        onChange={handleInputChange}
+      />
+      <label>Email:</label>
+      <input
+        type="email"
+        name="email"
+        value={newContact.email}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleAddContact}>Add Contact</button>
     </div>
   );
-}
+};
 
-export default App;
+export default ContactApp;
